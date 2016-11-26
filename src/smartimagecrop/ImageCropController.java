a0376da6d725ae6c4f42e75038a30100f26c7ca1
i340824 <Konijntje15@gmail.com>
2016-11-26 17:53:52 +0100

    Added select folder dialog

--- src/smartimagecrop/ImageCropController.java
+++ src/smartimagecrop/ImageCropController.java
@@ -5,24 +5,33 @@
  */
 package smartimagecrop;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Sander
  */
-public class ImageCropController implements Initializable{
+public class ImageCropController implements Initializable, IControllerParent{
     
     @FXML
-    private Button btnLoadImages;
-    
+    private Label laLoaded;
     @FXML
     private Button btnCrop;
+    
+    private Stage stage;
+    private Application application;
+    
+    private final static String NotLoadedText = "Images were not loaded.";  
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -31,13 +40,31 @@ public class ImageCropController implements Initializable{
     
     @FXML
     private void btnLoadImagesClick(ActionEvent event){
-        System.out.println("load");
-        btnCrop.setDisable(false);
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Directory Picker");
+        File imageFolder = directoryChooser.showDialog(stage);
+        
+        btnCrop.setDisable(imageFolder == null);
+        
+        if (imageFolder != null){
+            
+        }else
+            laLoaded.setText(NotLoadedText);
     }
     
     @FXML
     private void btnCropClick(ActionEvent event){
         System.out.println("crop");
+    }
+
+    @Override
+    public void setControllerParent(Stage stage) {
+        this.stage = stage;
+    }
+
+    @Override
+    public void shareApplication(Application application) {
+        this.application = application;
     }
             
     
